f9e38d6bc29d275794c3711054ee87f603efc411
Qiansimin <qiansimin@shining3d.com>
2018-09-07 14:59:33 +0800

    fix

--- src/MagicMoving.js
+++ src/MagicMoving.js
@@ -12,6 +12,7 @@ import {
   View,
   Modal,
   FlatList,
+  Image,
   TouchableOpacity,
   ScrollView,
   UIManager,  //UI测量
@@ -29,8 +30,12 @@ export default class MagicMoving extends Component {
     this._cardRefs = []  //保存Item的ref集合
     this.popupAnimatedValue = new Animated.Value(0) //设置动画初始值
     this.bannerImageAnimatedValue = new Animated.Value(0)  //banner的图片的动画
+    this.closeAnimatedvAalue = new Animated.Value(0)  //关闭按钮的淡入
     this._popupLayerStyle = null  //弹框动画的样式集合
     this._bannerImageStyle = null  //弹框内部banner的样式
+    this._closeBtnStyle = null
+
+    this._onPressCloseHanlder = this._onPressCloseHanlder.bind( this )
   }
   //最终的渲染
   render() {
@@ -59,16 +64,22 @@ export default class MagicMoving extends Component {
             width: Utils.interpolate(this.popupAnimatedValue, [0, 1], [width, DeviceSize.WIDTH]),
             height: Utils.interpolate(this.popupAnimatedValue, [0, 1], [height, DeviceSize.HEIGHT])
         }
-
         this._bannerImageStyle = {
             width: Utils.interpolate( this.bannerImageAnimatedValue, [0, 1], [ width,  DeviceSize.WIDTH] ),
             height: Utils.interpolate( this.bannerImageAnimatedValue, [0, 1], [ height,  DeviceSize.WIDTH * height / width ] ),
+        }
+        this._closeBtnStyle = {
+            justifyContent: 'center',
+            alignItems: 'center',
+            position: 'absolute',
+            top: 30,
+            right: 30,
+            opacity: Utils.interpolate( this.closeAnimatedvAalue, [0, 1], [0,1] )
         }
   }
 
 //单个Item点击事件
   _onPressItem = ( index ) => {
-      console.log( index )
     //   this.popupAnimatedValue.setValue( 0 )
     // UIManager.measure( node, ( x, y. width, height, pageX, pageY ) => {} ) 获得页面信息  Node:节点  x,y, width, height, pageX, pageY 分别是相对父组件的X坐标 Y坐标 节点宽高  节点相对屏幕X，Y坐标
     //findNodeHandle 找节点
@@ -88,7 +99,11 @@ export default class MagicMoving extends Component {
                 Animated.spring( this.popupAnimatedValue, {toValue: 1, friction: 2, tension: 7, duration: 5000 }),
                 Animated.spring( this.bannerImageAnimatedValue,  {toValue: 1, friction: 2, tension: 7, duration: 5000 } )
             ] ).start( () => {
-                console.log( this._bannerImageStyle )
+                Animated.sequence([
+                    Animated.timing( this.closeAnimatedvAalue, { toValue: 1, duration: 1000 } )
+                ]).start( () => {
+                    console.log('全部完成')
+                } )
             } )
              console.log( this._popupLayerStyle );
         })
@@ -106,10 +121,11 @@ export default class MagicMoving extends Component {
             ref = { _ => this._cardRefs[index] = _ }
             onPress = { () => this._onPressItem( index ) }
         >
-            <View>
+            <View style={ styles.item }>
+                <Image source={ { uri: item.image } } style={ styles.itemImage }/>
                 <Text>
-                    1111
-                </Text>
+                    { item.name }
+                </Text>    
             </View>
         </TouchableOpacity>
     )
@@ -151,6 +167,30 @@ export default class MagicMoving extends Component {
     )
   }
 
+  //关闭按钮渲染
+  _renderClose () {
+    return (
+        <Animated.View style={ this._closeBtnStyle }>
+            <TouchableOpacity style={ styles.closeContainer } onPress={ this._onPressCloseHanlder }>
+                <View style={[styles.forkLine, {top: +.5, transform: [{rotateZ: '45deg'}]}]}/>
+                <View style={[styles.forkLine, {top: -.5, transform: [{rotateZ: '-45deg'}]}]}/>
+            </TouchableOpacity>
+        </Animated.View>
+    )
+  }
+
+  //触发关闭
+  _onPressCloseHanlder () {
+    Animated.parallel( [
+        //保持同步的插值 视觉统一
+        Animated.spring( this.popupAnimatedValue, {toValue: 0}),
+        Animated.spring( this.bannerImageAnimatedValue,  {toValue: 0} ),
+        Animated.timing( this.closeAnimatedvAalue, { toValue: 0} )
+    ] ).start( () => {
+        this.setState({showPopupLayer: false});
+    } )
+  }
+
   //浮层内部内容
   _renderPopupLayerContent ( item, index ) {
       //banner和数据内容
@@ -158,10 +198,11 @@ export default class MagicMoving extends Component {
     return (
         <ScrollView>
             <Animated.Image source={ { uri: item.image } } style={ this._bannerImageStyle }/>
+            { this._renderClose() }
             <View>
                 <Text>
-                    asdasd
-                </Text>    
+                    { item.name }
+                </Text>
             </View>
         </ScrollView>
     )
@@ -171,10 +212,26 @@ export default class MagicMoving extends Component {
 const styles = StyleSheet.create({
 //绝对布局 脱离文档流 好运动
 popupLayer: { 
-    height: 20,
-    width: 5,
     backgroundColor: '#02DF82',
     position: 'absolute',
     overflow: 'hidden'
-  }
-});
+},
+closeContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 16,
+    height: 16,
+    borderRadius: 8,
+    backgroundColor: '#666'
+},
+forkLine: {
+    width: 10,
+    height: 1,
+    backgroundColor: '#FFF'
+},
+item: {
+},
+itemImage: {
+    height:50
+}
+})
